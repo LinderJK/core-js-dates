edf5dac7bcd34a3bfe084a38c874d0a3496ca1f7
dcswiki <dcsoyuz@gmail.com>
2024-03-25 14:51:28 +0300

    fix: formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -147,20 +147,17 @@ function isDateInPeriod(date, period) {
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
 function formatDate(date) {
-  const newDate = new Date(date);
-  const month = newDate.getMonth() + 1;
-  const day = newDate.getDate();
-  const year = newDate.getFullYear();
-  const hours = newDate.getHours();
-  const minutes = newDate.getMinutes();
-  const seconds = newDate.getSeconds();
-  const half = hours >= 12 ? 'PM' : 'AM';
-  const formattedHours = String(hours % 12 || 12).padStart(2, '0');
-  const formattedMinutes = String(minutes).padStart(2, '0');
-  const formattedSeconds = String(seconds).padStart(2, '0');
-  const formattedDate = `${month}/${day}/${year}, ${formattedHours}:${formattedMinutes}:${formattedSeconds} ${half}`;
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true,
+  };
 
-  return formattedDate;
+  return date.toLocaleString('en-US', options);
 }
 
 /**
